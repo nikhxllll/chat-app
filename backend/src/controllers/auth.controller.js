c4e057c5388d13950ac098598b5a812b3d54334d
Nikhil Yadav <ny619619@gmail.com>
2025-06-22 00:53:51 +0530

    password hashing using bcrypt

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,36 @@
-export const signup = (req,res)=>{
-    res.send("signup")
-}
+import User from "../model/user.model.js"
+import bcrypt from "bcryptjs"
+export const signup = async (req,res)=>{
+    const {fullName,email,password} = req.body;
+    try {
+        if (password.length < 6){
+            return res.status(400).json({message:"Password must be atleast 6 characters"})
+        }
+        const user = await User.findOne({email})
+        if (user){
+            res.status(400).json({message : "Email already exists"})
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password : hashedPassword
+        })
+
+        if (newUser){
+            // generate jwt tokens
+
+        }else{
+            res.status(400).json({message : "Invalid User Data"})
+        }
+        
+    }
+    } catch (error) {
+        
+    }
 export const login = (req,res)=>{
     res.send("login")
 
