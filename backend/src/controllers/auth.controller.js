eb48138072a1d75c5bcc4a937eaf9391845cb6d6
Nikhil Yadav <ny619619@gmail.com>
2025-06-22 01:25:19 +0530

    signup controller completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,6 +5,11 @@ import bcrypt from "bcryptjs"
 export const signup = async (req,res)=>{
     const {fullName,email,password} = req.body;
     try {
+        if (!fullName || !email || !password){
+            return res
+              .status(400)
+              .json({ message: "All fields are required" });
+        }
         if (password.length < 6){
             return res.status(400).json({message:"Password must be atleast 6 characters"})
         }
