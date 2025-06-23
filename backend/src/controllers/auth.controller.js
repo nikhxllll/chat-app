7e13fc0a2d8ec7b9eec3e73f8d720bfc208be922
Nikhil Yadav <ny619619@gmail.com>
2025-06-23 23:30:54 +0530

    Update Profile controller completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import { json } from "express";
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../model/user.model.js"
 import bcrypt from "bcryptjs"
@@ -75,7 +77,7 @@ export const login = async (req,res)=>{
     }
 
 }
-export const logout = async (req,res)=>{
+export const logout = (req,res)=>{
     try {
         res.cookie("jwt","",{maxAge: 0});
         res.status(200).json({message:"Logout Successfully"})
@@ -86,5 +88,28 @@ export const logout = async (req,res)=>{
 
 }
 export const updateProfile = async (req,res) =>{
-    
+    try {
+        const {profilePic} = req.body
+        const userId = req.user._id
+
+        if (!profilePic){
+            res.status(400).json({
+                message:"Profile Pic is required"
+            })
+        }
+
+        const uploadResponse = cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            {profilePic:uploadResponse.secure_url},
+            {new: true}
+        );
+
+        res.status(200).json(updatedUser)
+
+    } catch (error) {
+        console.log("Error in Update User controller",error.message)
+        res.status(500).json({message : " Internal Server Error"})
+        
+    }
 }
