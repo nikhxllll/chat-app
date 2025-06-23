756dbdb1f263230d23afe1bfb7aeed434164c993
Nikhil Yadav <ny619619@gmail.com>
2025-06-23 23:05:20 +0530

    update Profile Controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,7 +75,7 @@ export const login = async (req,res)=>{
     }
 
 }
-export const logout = (req,res)=>{
+export const logout = async (req,res)=>{
     try {
         res.cookie("jwt","",{maxAge: 0});
         res.status(200).json({message:"Logout Successfully"})
@@ -84,4 +84,7 @@ export const logout = (req,res)=>{
         res.status(500).json({message:"Internal Server Error"})
     }
 
+}
+export const updateProfile = async (req,res) =>{
+    
 }
