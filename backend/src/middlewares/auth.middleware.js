6d7e689d0432bfca54eb24c1a2f4d8868bc83522
Nikhil Yadav <ny619619@gmail.com>
2025-06-23 22:59:22 +0530

    checking of authentication

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt, { decode } from "jsonwebtoken"
+import User from "../model/user.model.js"
+
+export const protectRoute = async (req,res) =>{
+    try {
+        const token = req.cookie.jwt
+    
+        if (!token){
+            res.status(401).json({message: "Unauthorized - No Token Provided!"})
+        }
+    
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+
+        if (!decoded){
+            res.status(401).json({message: "Unauthorized - Invalid Token"})
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+        if (!user){
+            res.status(404).json({message: "User Not Found"})
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        console.log("Error in protectRoute Middleware",error.message)
+        res.status(500).json({message:"Internal Server Error!"})
+    }
+}
