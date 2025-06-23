b42f85bfb9b9e8388df5f3b6fe6065b693c649d3
Nikhil Yadav <ny619619@gmail.com>
2025-06-23 22:56:03 +0530

    cookie-perser imported

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
-
 import authRoutes from "./routes/auth.routes.js"
 import { connectDB } from "./lib/db.js"
+import cookieParse from "cookie-parser"
 
 const app = express();
 
-dotenv.config()
-app.use(express.json())
-
-const PORT = process.env.PORT || 5000
+dotenv.config();
+app.use(express.json());
+app.use(cookieParse());
 
 app.use("/api/auth",authRoutes)
 
