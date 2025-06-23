c346ecb218b0cb1e0542e6d95fc807109fbad7c4
Nikhil Yadav <ny619619@gmail.com>
2025-06-23 23:34:42 +0530

    checkAuth completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -117,6 +117,6 @@ export const checkAuth = (req,res)=>{
         res.status(200).json(req.user)
     } catch (error) {
         console.log("Error in checkAuth Controller",error.message)
-        res.status(500).json({message:"Internal Server Error})
+        res.status(500).json({message:"Internal Server Error"})
     }
 }
