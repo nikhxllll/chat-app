9e41d512a0fee7dd6936a358e7aecc2adee19e6b
Nikhil Yadav <ny619619@gmail.com>
2025-06-24 13:41:35 +0530

    chnges acc to message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import { connectDB } from "./lib/db.js"
 import cookieParse from "cookie-parser"
+import messageRoutes from "./routes/message.routes.js"
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParse());
 
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 app.listen(PORT,()=>{
     console.log("Server is running on Port",PORT)
