e7c2bc99ae1801fb3c33cc7e8dd300e6b7d4fd4c
Nikhil Yadav <ny619619@gmail.com>
2025-06-24 11:15:16 +0530

    Route of checkAuth added

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { signup,login,logout } from "../controllers/auth.controller.js";
+import { signup,login,logout,updateProfile,checkAuth } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+// import { updateProfile } from "../controllers/auth.controller.js";
 const router = express.Router()
 
 router.post("/signup",signup)
