db1aab4fd907f46fde2305ba90e9591b98f7b376
Nikhil Yadav <ny619619@gmail.com>
2025-06-25 10:32:43 +0530

    get messages of users

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -11,7 +11,16 @@ export const getUsersForSidebar = async (req,res)=>{
 }
 export const getMessages = async (req,res)=>{
     try {
-        
+        const {id :userToChatId} = req.params
+        const myId = req.user._id
+
+        const messages = await MessageChannel.find({
+            $or:[
+                {senderId: myId, receiverId: userToChatId},
+                {senderId: userToChatId, receiverId: myId},
+            ],
+        })
+        res.status(200).json(messages)
     } catch (error) {
         console.log("Error In getMesssages Controller",error.message)
         res.status(500).json({error: "Internal Server Error"})
