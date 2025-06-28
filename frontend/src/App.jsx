8ee4083a3e47b999359029993f1b093668589aca
Nikhil Yadav <ny619619@gmail.com>
2025-06-28 12:18:53 +0530

    checked the comp of daisy ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-red-300'>
-      Hello World
+    <div >
+      
     </div>
-  )
+  );
 }
 
 export default App
