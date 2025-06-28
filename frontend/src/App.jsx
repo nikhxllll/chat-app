8d25151707fdd67426146fe0eeb0e976e36fc246
Nikhil Yadav <ny619619@gmail.com>
2025-06-28 22:24:52 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Navbar from './comonents/Navbar';
+import {Routes,Route} from "react-router-dom";
 const App = () => {
   return (
     <div >
-      
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/signup' element={<SignUpPage />}/>
+          <Route path='/login' element={<LoginPage />}/>
+          <Route path='/settings' element={<SettingsPage />}/>
+          <Route path='/profile' element={<ProfilePage />}/>
+
+        </Routes>
     </div>
   );
 }
