cae8b8e5f0a27d0e59d5f37063d6d8844cdcdd58
Nikhil Yadav <ny619619@gmail.com>
2025-06-28 22:26:11 +0530

    Routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,13 @@ const App = () => {
   return (
     <div >
       <Navbar />
-        <Routes>
-          <Route path='/' element={<HomePage />}/>
-          <Route path='/signup' element={<SignUpPage />}/>
-          <Route path='/login' element={<LoginPage />}/>
-          <Route path='/settings' element={<SettingsPage />}/>
-          <Route path='/profile' element={<ProfilePage />}/>
-
-        </Routes>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='/signup' element={<SignUpPage />}/>
+        <Route path='/login' element={<LoginPage />}/>
+        <Route path='/settings' element={<SettingsPage />}/>
+        <Route path='/profile' element={<ProfilePage />}/>
+      </Routes>
     </div>
   );
 }
