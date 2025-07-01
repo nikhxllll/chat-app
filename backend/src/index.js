e142b8344ee276dce54a48072de038691b6bf83d
Nikhil Yadav <ny619619@gmail.com>
2025-07-01 23:20:31 +0530

    cors implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.routes.js"
 import { connectDB } from "./lib/db.js"
 import cookieParse from "cookie-parser"
 import messageRoutes from "./routes/message.routes.js"
+import cors from "cors"
 
 const app = express();
 
@@ -12,6 +13,10 @@ dotenv.config();
 const PORT = process.env.PORT
 app.use(express.json());
 app.use(cookieParse());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
