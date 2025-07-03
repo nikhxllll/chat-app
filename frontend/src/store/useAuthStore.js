58c99306c0f8d503929770242019e5a029f7665c
Nikhil Yadav <ny619619@gmail.com>
2025-07-03 16:51:32 +0530

    sign up complete

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import {create} from "zustand"
 import { axiosInstance } from "../lib/axios"
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set)=>({
     authUser : null,
@@ -20,9 +21,19 @@ export const useAuthStore = create((set)=>({
         }finally{
             set({isCheckingAuth:false})
         }
-    }
-    signup: async (data)=>{
-
-    }
+    },
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+          const res = await axiosInstance.post("/auth/signup", data);
+          set({ authUser: res.data });
+          toast.success("Account created successfully");
+        //   get().connectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isSigningUp: false });
+        }
+    },
 
 }))
