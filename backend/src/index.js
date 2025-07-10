8be833dfec7c8f3212a721f0f741a566583d42b4
Nikhil Yadav <ny619619@gmail.com>
2025-07-10 17:40:33 +0530

    changed acc to socket config

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,8 @@ import { connectDB } from "./lib/db.js";
 import cookieParse from "cookie-parser";
 import messageRoutes from "./routes/message.routes.js";
 import cors from "cors";
+import {app,server} from "./lib/socket.js"
 
-const app = express();
 
 dotenv.config();
 
@@ -23,7 +23,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server is running on Port", PORT);
   connectDB();
 });
